3031695f490bfccb6d70f8a0bf0d2402d1ab04e8
Obed Bien-aime <bien0025@algonquinlive.com>
2025-02-10 11:46:50 -0500

    bobobo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+//https://github.com/bien0025/improved-goggles
+//bien0025.github.io/improved-goggles/index.html
+//http://127.0.0.1:5502/index.html
+//https://improved-goggles-gules.vercel.app/
+
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
